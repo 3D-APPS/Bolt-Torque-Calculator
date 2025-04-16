e5afd9c612bbf461fb88f720e701378e65b38263
Darrell Dunn <damanddd@gmail.com>
2025-04-16 08:12:46 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = 'bolt-torque-calculator-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './styles.css', // Include your CSS file if separate
+  './service-worker.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
